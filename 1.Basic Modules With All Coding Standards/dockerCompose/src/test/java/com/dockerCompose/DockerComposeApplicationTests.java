88f3045f5c1ea2ce09db03ea2132b49124900236
ajit <ajinathp.guru@gmail.com>
2025-12-16 01:05:01 +0530

    1. commit

---
+++ 1.Basic Modules With All Coding Standards/dockerCompose/src/test/java/com/dockerCompose/DockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerCompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
